8d02e46a31b6c4fdee1da5961d9567e5922a35e5
Valery Romanov <ikrakozabra@gmail.com>
2016-12-19 15:31:11 +0300

    [Change] убраны ненужные свойства из объекта PARAMS

--- twitch-api-commons.js
+++ twitch-api-commons.js
@@ -1,14 +1,14 @@
+/**
+ * Объект, содержащий все необходимые расширению методы для работы с Twitch API
+ */
 var Twitch = (function($) {
 'use strict';
     const USER  = 'world_enslaver'; // пока используем не-кастомного юзера
     const PARAMS = {
         'client_id' : '<CLIENT_ID>',
-        'state' : (Math.random()*1000).toFixed(0), // wanna-be генерация токена
-        'scope' : 'user_subscriptions',
-        'redirect_uri'  : 'localhost',
-        'response_type' : 'code',
     }
 
+    // перехватываем все исходящие AJAX-запросы и добавляем в них заголовок Client-ID
     $.ajaxSetup({
         beforeSend : request => request.setRequestHeader('Client-ID', PARAMS.client_id)
     });
@@ -16,6 +16,7 @@ var Twitch = (function($) {
     return {
         /**
          * Получает список из подписок текущего пользователя
+         * @returns {jqXHR}
          */
         getFollowingList : function() {
             return $.get('https://api.twitch.tv/kraken/users/'+ USER +'/follows/channels')
@@ -24,6 +25,7 @@ var Twitch = (function($) {
         /**
          * Получает список стримов
          * @param channels {String} каналы, для которых хотим получить стримы (через ',')
+         * @returns {jqXHR} 
          */
         getStreamList : function(channels) {
             return $.get('https://api.twitch.tv/kraken/streams', {channel : channels});
